7ed367fe9c5373fdf2cad173c7ba1a5e47d7c102
Livinho Martins <mlivinhors@gmail.com>
2024-10-13 02:57:17 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Olá amorzinho!! Aqui é o Lukas, seu amoh ihuul. Esse é um site que fiz especialmente para você.</h1>
+    return <h1>Olá amorzinho!! Aqui é o Lukas, seu amoh ihuul. Esse é um site que fiz especialmente para você, ihul.</h1>
 }
 
 export default Home;
