213d8f1fa04335c459b968e90aa2e268e5cbc2f2
Livinho Martins <mlivinhors@gmail.com>
2025-05-20 15:43:14 +0000

    fix: create new migration and fix a bug in npm run dev

---
+++ testes/integration/api/v1/users/post.test.js
@@ -0,0 +1,17 @@
+import orchestrator from "testes/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+});
+
+describe("POST /api/v1/users", () => {
+  describe("Anonymous user", () => {
+    test("With unique and valid data", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+      });
+      expect(response.status).toBe(201);
+    });
+  });
+});
